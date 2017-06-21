97ef9ddb5bf3b36bee1389f0c01eeedba90f05ee
xtfgq <gq820125@163.com>
2017-06-21 17:02:46 +0800

    修改完成

--- app/src/main/java/com/goodfriend/app/ui/activity/MainActivity.java
+++ app/src/main/java/com/goodfriend/app/ui/activity/MainActivity.java
@@ -1,11 +1,25 @@
 package com.goodfriend.app.ui.activity;
 
 
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 import com.goodfriend.R;
+import com.goodfriend.app.ui.view.ImageCycleView;
+import com.goodfriend.app.utils.ScreenUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
@@ -13,11 +27,17 @@ import butterknife.OnClick;
  */
 
 public class MainActivity extends BaseAppCompatActivity {
+    //广告轮播 图片链接
+    ArrayList<String> mImageUrl = null;
+    //广告通用对象
+    ArrayList<Map<String, Object>> mObject = null;
     @BindView(R.id.tv_search_bg)
     TextView tvSearchBg;
 
     @BindView(R.id.activity_ele_search)
     LinearLayout activityEleSearch;
+    @BindView(R.id.viewPager)
+    ImageCycleView viewPager;
 
     @Override
     public int getLayoutId() {
@@ -32,6 +52,38 @@ public class MainActivity extends BaseAppCompatActivity {
     @Override
     public void initView() {
         setTitle("良友排号");
+        mImageUrl=new ArrayList<String>();
+        mImageUrl.add("http://ehome.staging.topmd.cn:81/" +
+                "ueditor/net/upload/image/20160926/6361050831635160295248500.jpg");
+        mImageUrl.add("http://ehome.staging.topmd.cn:81/ueditor/net/upload/image" +
+                "/20160926/6361050887459653274633315.jpg");
+        mImageUrl.add("http://ehome.staging.topmd.cn:81/ueditor/net/upload/image/20160926/6361050921977527587668365.jpg");
+        mObject=new ArrayList<Map<String, Object>>();
+        for(int i=0;i<3;i++) {
+            Map<String, Object> map;
+            map = new HashMap<String, Object>();
+            map.put("ID", i);
+            mObject.add(map);
+        }
+
+        viewPager.setImageResources(mImageUrl,mObject,
+                new ImageCycleView.ImageCycleViewListener(){
+            @Override
+            public void displayImage(String imageURL, ImageView imageView) {
+                Glide.with(MainActivity.this).load(imageURL).into(imageView);
+                ViewGroup.LayoutParams para;
+                para = imageView.getLayoutParams();
+                para.width = ScreenUtils.getScreenWidth(MainActivity.this);
+                para.height = para.width * 2 / 5;
+                imageView.setLayoutParams(para);
+            }
+
+            @Override
+            public void onImageClick(int position, View imageView, ArrayList<Map<String,
+                    Object>> mObject) {
+
+            }
+        });
     }
 
 
@@ -39,6 +91,7 @@ public class MainActivity extends BaseAppCompatActivity {
     public void onViewClicked() {
 
     }
+
 
 
 }
